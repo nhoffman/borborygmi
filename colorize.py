dcd391a8d90cc116f7465fbd27ba97507f0ec84c
Noah Hoffman <noah.hoffman@gmail.com>
2014-05-29 18:07:27 -0700

    add script to colorize exported html

---
+++ colorize.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+"""
+Colorize source-code blocks using pygments.
+"""
+
+import os
+import sys
+import argparse
+import re
+
+from lxml import html
+
+from pygments import highlight
+from pygments.lexers import get_all_lexers, get_lexer_by_name
+from pygments.formatters import get_formatter_by_name
+
+
+def main(arguments):
+
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('infile', help = "Input file name")
+    parser.add_argument('-o', '--outfile',
+                        help = "Output file name (replaces infile by default)")
+    parser.add_argument('-L', '--list-lexers', action='store_true', default=False)
+
+    args = parser.parse_args(arguments)
+
+    if args.list_lexers:
+        for x in get_all_lexers():
+            print x
+        sys.exit()
+
+    # associate css-classes in code blocks with pygemnts lexer names
+    lexers = {'src-python': 'python',
+              'src-sh': 'bash',
+              'src-sqlite': 'sql',
+              'emacs-lisp': 'cl'}
+
+    formatter = get_formatter_by_name('html')
+
+    with open(args.infile) as f:
+        tree = html.fromstring(f.read())
+
+    code_blocks = tree.xpath('//pre')
+    for block in code_blocks:
+        css_class = block.get('class').split()[-1]
+
+        try:
+            lexer = get_lexer_by_name(lexers[css_class])
+        except KeyError:
+            continue
+
+        colorized = html.fragment_fromstring(
+            highlight(block.text, lexer, formatter),
+            create_parent=False)
+
+        parent = block.getparent()
+        parent.replace(block, colorized)
+
+    with open(args.outfile or args.infile, 'w') as f:
+        f.write(html.tostring(tree))
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
